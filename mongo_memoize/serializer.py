0e35b64f88c57d56e0397e90658c3c47e33217d5
William Bernoudy <wbernoudy@dwavesys.com>
2018-05-22 12:53:50 -0700

    Add python 3 compatibility

--- mongo_memoize/serializer.py
+++ mongo_memoize/serializer.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import base64
-import cPickle as pickle
+import sys
+
+if sys.version_info[0] == 2:
+    import cPickle as pickle
+else:
+    import pickle
 
 
 class NoopSerializer(object):

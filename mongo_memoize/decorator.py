14c10d9e97697d57b9eb740633010906675f9ecb
Jamie King <jking@dwavesys.com>
2018-08-23 16:17:45 -0700

    Wrote batch processing function.  Untested.

--- mongo_memoize/decorator.py
+++ mongo_memoize/decorator.py
@@ -77,27 +77,6 @@ class Memoizer(object):
         cache_col.ensure_index('key', unique=True)
         return cache_col
 
-    def batch_find_keys(self, func, arg_list=None, kwarg_list=None):
-        """Check lists of args and kwargs to see if they've been calculated."""
-
-        if arg_list is None and kwarg_list is None:
-            return list()
-
-        if arg_list is None:
-            arg_list = [() for _ in kwarg_list]
-
-        if kwarg_list is None:
-            if len(arg_list) and isinstance(arg_list[0], dict):
-                raise ValueError("Dictionary provided in arg_list.  If only kwarg_list is to be provide it, "
-                                 "provide it as a named argument.")
-            kwarg_list = [dict() for _ in arg_list]
-
-        key_list = [self.key_generator(args, kwargs) for args, kwargs in zip(arg_list, kwarg_list)]
-        cache_col = self.initialize_col(func)
-        key_found = [bool(cache_col.find_one(dict(key=cache_key))) for cache_key in key_list]
-        self.disconnect()
-        return key_found
-
     def memoize(self):
 
         """A decorator that caches results of the function in MongoDB.
@@ -168,6 +147,115 @@ class Memoizer(object):
             return wrapped_func
 
         return decorator
+
+    @staticmethod
+    def normalize_args_list(arg_list, kwarg_list):
+        if arg_list is None and kwarg_list is None:
+            return list()
+
+        if arg_list is None:
+            arg_list = [() for _ in kwarg_list]
+
+        if kwarg_list is None:
+            if len(arg_list) and isinstance(arg_list[0], dict):
+                raise ValueError("Dictionary provided in arg_list.  If only kwarg_list is to be provide it, "
+                                 "provide it as a named argument.")
+            kwarg_list = [dict() for _ in arg_list]
+
+        return arg_list, kwarg_list
+
+    def get_key_list(self, arg_list, kwarg_list):
+        arg_list, kwarg_list = self.normalize_args_list(arg_list, kwarg_list)
+        key_list = [self.key_generator(args, kwargs) for args, kwargs in zip(arg_list, kwarg_list)]
+        return key_list
+
+    def batch_check_keys(self, func, key_list):
+        """Check lists of args and kwargs to see if they've been calculated."""
+        cache_col = self.initialize_col(func)
+        key_found = [bool(cache_col.find_one(dict(key=cache_key))) for cache_key in key_list]
+        self.disconnect()
+        return key_found
+
+
+def batch_wrapper(decorator, func):
+    def wrapped(arg):
+        args, kwargs = arg
+        return decorator(func)(*args, **kwargs)
+    return wrapped
+
+
+def batch_process(memoizer, func, arg_list=None, kwarg_list=None, processes=1):
+
+    import logging
+    from multiprocessing import Pool
+    import tqdm
+    import pandas as pd
+
+    _logger = logging.getLogger(func.__name__)
+    _logger.info("Running script with {} processes.".format(processes))
+    _logger.debug("Checking for existing keys.")
+    key_list = memoizer.get_key_list(arg_list, kwarg_list)
+    key_found = memoizer.batch_find_keys(func, key_list)
+    _logger.info("Found {} out of {} keys in database.".format(sum(key_found), len(key_found)))
+
+    _logger.debug("Creating multiprocessing.Pool.")
+    p = Pool(processes=processes)
+
+    # Create a dataframe to store the parameter list.
+    arg_list, kwarg_list = memoizer.normalize_args_list(arg_list, kwarg_list)
+    df_args = pd.DataFrame(arg_list)
+    df_args.columns = ['args{}'.format(col) for col in df_args.columns]
+    df_kwargs = pd.DataFrame(kwarg_list)
+    df = pd.concat([df_args, df_kwargs], axis=1, sort=False)
+
+    # Add a column containing the database keys.
+    df['_key'] = key_list
+    df['_preexisting'] = key_found
+
+    # Get filtered lists of args and kwargs.
+    task_args = [arg_list[i] for i, b in enumerate(key_found) if not b]
+    task_kwargs = [kwarg_list[i] for i, b in enumerate(key_found) if not b]
+
+    # Run decorated function for incomplete keys.
+    if len(task_args):
+        _logger.info("Starting to compute new results.")
+        for _ in tqdm.tqdm(p.imap(batch_wrapper(memoizer.memoize(), func), zip(task_args, task_kwargs))):
+            pass
+        _logger.info("Finished computing new results.")
+
+    # Run again to gather all return values.
+    _logger.info("Starting to fetch all results.")
+    result_list = list(tqdm.tqdm(p.imap(batch_wrapper(memoizer.memoize(), func), zip(arg_list, kwarg_list))))
+    _logger.info("Done fetching all results.")
+
+    df_results = pd.DataFrame(result_list)
+    df = pd.concat([df, df_results], axis=1, sort=False)
+    return df
+
+
+# def func_wrapper(func, cache_col):
+#     """Function wrapper so that args and kwargs can be provided to """
+#
+#     def wrapped(arg):
+#         ret = func(*args, **kwargs)
+#         args, kwargs = arg
+#         cache_col.update(
+#             {'key': cache_key},
+#             {
+#                 '$set': {
+#                     'result': self.serializer.serialize(ret),
+#                     'args': str(args),
+#                     'kwargs': str(kwargs)
+#                 }
+#             },
+#             upsert=True
+#         )
+#
+#         return ret
+#
+#         return func(*args, **kwargs)
+#     return wrapped
+#
 
 
 def memoize(
